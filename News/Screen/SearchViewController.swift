a6f589cc7a39670eaa6360a75b76c9c2745b53b5
rex <rex>
2022-06-11 13:43:57 +0800

    #screen debug

--- News/Screen/SearchViewController.swift
+++ News/Screen/SearchViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 import Foundation
 class SearchViewController: UIViewController  {
-  
-    private let fullScreenSize = UIScreen.main.bounds.size
+    
+    private var fullScreenSize = UIScreen.main.bounds.size
     private var viewModel: SearchNewsViewModel
-    let TitleLabel = UILabel()
-    let SearchBar = UISearchBar()
-    var TableView = UITableView()
+    let TitleLabel : UILabel = {
+        let TitleLabel = UILabel()
+        TitleLabel.text = "搜尋"
+        TitleLabel.font = UIFont(name: "Thonburi-Bold", size: 24)
+        TitleLabel.tintColor = .systemBackground
+        TitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        return TitleLabel
+    }()
+    let SearchBar : UISearchBar = {
+        let SearchBar = UISearchBar()
+        SearchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        SearchBar.barTintColor = .systemFill
+        SearchBar.searchBarStyle = .minimal
+        SearchBar.searchTextField.backgroundColor = .systemFill
+        return SearchBar
+    }()
+    var TableView : UITableView = {
+        let TableView = UITableView()
+        TableView.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
+        TableView.contentInset =  UIEdgeInsets(top: -22, left: 0, bottom: 150, right: 0)
+        
+        TableView.separatorStyle = .singleLine
+        
+        
+        TableView.translatesAutoresizingMaskIntoConstraints = false
+        TableView.contentInset = UIEdgeInsets(top: 0,left: 0,bottom: 65 ,right: 0)
+        return TableView
+    }()
     var loadingView: LoadingView = LoadingView()
     let ErrorLabel = UILabel()
     
@@ -27,70 +53,67 @@ class SearchViewController: UIViewController  {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+    }
+    
     @objc func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        
         view.endEditing(true)
     }
-//    let SearchController = SearchViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
-        //MARK: Title Label
+        
+        
         self.view.addSubview(SearchBar)
         self.view.addSubview(TitleLabel)
-        TitleLabel.text = "搜尋"
-        TitleLabel.font = UIFont(name: "Thonburi-Bold", size: 24)
-        TitleLabel.tintColor = .systemBackground
-        TitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(TableView)
+        
+        
+        SearchBar.delegate = self
+        
+        self.view.addSubview(TableView)
+        TableView.delegate = self
+        TableView.dataSource = self
+        
+        
+        applyConstrant()
+        
+        
+        
+        //Looks for single or multiple taps.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    private func applyConstrant(){
+        //MARK: Title Label
+        
+        
         TitleLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
         TitleLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
         TitleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50).isActive = true
         TitleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-    
-       
-        //MARK: SearchBar Layout
-  
-        SearchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        //MARK: SearchBar
         SearchBar.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        SearchBar.widthAnchor.constraint(equalToConstant: fullScreenSize.width).isActive = true
+        //        SearchBar.widthAnchor.constraint(equalToConstant: fullScreenSize.width).isActive = true
         SearchBar.topAnchor.constraint(equalTo: TitleLabel.bottomAnchor, constant: 10).isActive = true
+        SearchBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
+        SearchBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
         SearchBar.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        SearchBar.barTintColor = .systemFill
-        SearchBar.searchBarStyle = .minimal
-        SearchBar.searchTextField.backgroundColor = .systemFill
-        SearchBar.delegate = self
-
-        //MARK: Search Table View config
-
-        self.view.addSubview(TableView)
-
-        TableView.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
-        TableView.contentInset =  UIEdgeInsets(top: -22, left: 0, bottom: 150, right: 0)
-        TableView.delegate = self
-        TableView.dataSource = self
-        TableView.separatorStyle = .singleLine
-       
-        self.view.addSubview(TableView)
-        TableView.translatesAutoresizingMaskIntoConstraints = false
-        TableView.contentInset = UIEdgeInsets(top: 0,left: 0,bottom: 65 ,right: 0)
+        //MARK: TableView
         NSLayoutConstraint(item: TableView, attribute: .top, relatedBy: .equal, toItem: self.SearchBar, attribute: .bottom, multiplier: 1.0, constant: 30).isActive = true
-        NSLayoutConstraint(item: TableView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: fullScreenSize.width).isActive = true
-        NSLayoutConstraint(item: TableView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: fullScreenSize.height-100).isActive = true
-        
-        
-        
-        
-        
-        
-    //Looks for single or multiple taps.
-     let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
-
-    //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
-    //tap.cancelsTouchesInView = false
-
-    view.addGestureRecognizer(tap)
+        //        NSLayoutConstraint(item: TableView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: fullScreenSize.width).isActive = true
+        //        NSLayoutConstraint(item: TableView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: fullScreenSize.height-100).isActive = true
+        NSLayoutConstraint(item: TableView, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: TableView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: TableView, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
     }
- 
+    
+    
     
     
     
@@ -110,12 +133,12 @@ class SearchViewController: UIViewController  {
         ErrorLabel.translatesAutoresizingMaskIntoConstraints = false
         ErrorLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0).isActive = true
         ErrorLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
-    
+        
     }
-
+    
 }
 extension SearchViewController: UISearchBarDelegate{
-   
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         self.configLoadingView()
         self.loadingView.activityIndicator.startAnimating()
@@ -123,24 +146,24 @@ extension SearchViewController: UISearchBarDelegate{
         temp = temp.replacingOccurrences(of: " ",with: "")
         self.viewModel.loadQuery(query: temp){
             DispatchQueue.main.async {
-           
+                
                 self.loadingView.activityIndicator.stopAnimating()
                 self.loadingView.removeFromSuperview()
                 if (self.viewModel.NewsList.count == 0 ){
                     self.configError()
                     
                 } else if self.ErrorLabel.isDescendant(of: self.view){
-                     
-                        self.ErrorLabel.removeFromSuperview()
+                    
+                    self.ErrorLabel.removeFromSuperview()
                     
                 }
                 self.TableView.reloadData()
             }
             
         }
-  
-  
-      }
+        
+        
+    }
 }
 
 
@@ -149,7 +172,7 @@ extension SearchViewController: UISearchBarDelegate{
 
 extension SearchViewController:UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-     
+        
         
         return self.viewModel.NewsList.count > 30 ? 30 : self.viewModel.NewsList.count
     }
@@ -160,18 +183,18 @@ extension SearchViewController:UITableViewDelegate,UITableViewDataSource{
             withIdentifier: "SearchTableViewCell", for: indexPath) as! SearchTableViewCell
         
         cell.config(new: self.viewModel.NewsList[indexPath.row])
-      
-    
-              return cell
         
-          
+        
+        return cell
+        
+        
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200
     }
     
-
-   
-
+    
+    
+    
 }
